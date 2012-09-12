be5d189705484ba7f517313c7ec5f16b72a80da0
Peter <peterarenot@gmail.com>
2012-09-12 18:36:22 +1000

    Created fixture app

---
+++ views.py
@@ -0,0 +1,8 @@
+from fixtures.models import *
+from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.template import Context, loader, RequestContext
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/login/')
+def manage(request):
+    return render_to_response('fixtures/manage.html')
